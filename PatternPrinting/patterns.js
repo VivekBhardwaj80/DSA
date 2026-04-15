3eda21dc21b2f989d198b42b445ff5b659d1f884
Vivek Bhardwaj <vp0444199@gmail.com>
2026-04-16 00:26:17 +0530

    starting my DSA journey

---
+++ PatternPrinting/patterns.js
@@ -0,0 +1,58 @@
+// PATTERN 1
+{
+  //   let n = 4;
+  //   for (let i = 1; i <= n; i++) {
+  //     for (let j = 1; j <= n; j++) {
+  //       process.stdout.write("* ");
+  //     }
+  //     console.log();
+  //   }
+}
+
+// PATTERN 2
+{
+  //   let n = 5;
+  //   let m = 3;
+  //   for (let i = 1; i <= m; i++) {
+  //     for (let j = 1; j <= n; j++) {
+  //       process.stdout.write("* ");
+  //     }
+  //     console.log();
+  //   }
+}
+
+// PATTERN 3
+{
+  // let n =5
+  // for(let i=1; i<=n; i++){
+  //     for(let j=1; j<=i; j++){
+  //         process.stdout.write("* ")
+  //     }
+  //     console.log()
+  // }
+}
+
+// PATTERN 4
+{
+  //   let n = 5;
+  //   for (let i = n; i >= -1; i--) {
+  //     for (let j = 1; j <= i; j++) {
+  //       process.stdout.write("* ");
+  //     }
+  //     console.log();
+  //   }
+}
+
+// PATTERN 5
+{
+  //   let n = 10;
+  //   for (let row = 1; row <= n; row++) {
+  //     for (let col = 1; col <= n - row; col++) {
+  //       process.stdout.write(" ");
+  //     }
+  //     for (let col = 1; col < n; col++) {
+  //       process.stdout.write("* ");
+  //     }
+  //     console.log();
+  //   }
+}
